57cba725f17d9a46a70bae2d4fd9283c2d1b130e
wahab <abdulwahab209.dev@gmail.com>
2026-05-20 17:46:22 +0500

    fixed cors and root route

--- index.js
+++ index.js
@@ -6,7 +6,10 @@ const app = express();
 
 const cors = require("cors");
 
-app.use(cors({ origin: "https://bank-app-fe.vercel.app/", credentials: true }));
+app.use(cors({
+    origin: "https://bank-app-fe.vercel.app",
+    credentials: true
+}));
 
 app.use(express.json());
 
@@ -29,13 +32,15 @@ app.use("/api/admin", adminDashboardRoute);
 const cardRoute = require("./routes/card");
 app.use("/api/card", cardRoute);
 
-// Public Rates API for Home Page
+app.get("/", (req, res) => {
+    res.send("Backend is running successfully");
+});
+
+// Public Rates API
 app.get("/api/rates", (req, res) => {
     res.json([
         { _id: '1', type: 'Saving Account', rate: '12.5%', period: 'Per Annum' },
         { _id: '2', type: 'Current Account', rate: '0.0%', period: 'N/A' },
-        { _id: '3', type: 'Fixed Deposit (1yr)', rate: '15.2%', period: 'Mature' },
-        { _id: '4', type: 'Student Account', rate: '8.0%', period: 'Per Annum' }
     ]);
 });
 
