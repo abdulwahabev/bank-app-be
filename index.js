d5a6933096a2067872a36bfd04b3f08ee8949157
wahab <abdulwahab209.dev@gmail.com>
2026-05-20 19:42:34 +0500

    message

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const app = express();
 const cors = require("cors");
 
 app.use(cors({
-    origin: "https://bank-app-fe.vercel.app",
+    origin: ["https://bank-app-fe.vercel.app", "http://localhost:5173", "http://localhost:3000"],
     credentials: true
 }));
 
@@ -49,3 +49,5 @@ const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
+
+module.exports = app;
